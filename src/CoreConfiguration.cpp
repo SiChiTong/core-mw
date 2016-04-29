9d76236a4065848cc53df35355e7b91b2d518fc3
Davide Rizzi <d.rizzi@novalabs.io>
2016-04-29 09:47:09 +0200

    Added Copyright statement

--- src/CoreConfiguration.cpp
+++ src/CoreConfiguration.cpp
@@ -1,3 +1,9 @@
+/* COPYRIGHT (c) 2016 Nova Labs SRL
+ *
+ * All rights reserved. All use of this software and documentation is
+ * subject to the License Agreement located in the file LICENSE.
+ */
+ 
 #include <Core/MW/CoreConfiguration.hpp>
 
 NAMESPACE_CORE_MW_BEGIN
--- src/Middleware.cpp
+++ src/Middleware.cpp
@@ -1,3 +1,9 @@
+/* COPYRIGHT (c) 2016 Nova Labs SRL
+ *
+ * All rights reserved. All use of this software and documentation is
+ * subject to the License Agreement located in the file LICENSE.
+ */
+ 
 #include <Core/MW/namespace.hpp>
 #include <Core/MW/Middleware.hpp>
 #include <Core/MW/NamingTraits.hpp>
