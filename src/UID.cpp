93dcbc32a7451a9fcdb61b5ea0bef7bb72cdcfba
Davide Rizzi <d.rizzi@novalabs.io>
2016-06-15 17:32:27 +0200

    Added UID

---
+++ src/UID.cpp
@@ -0,0 +1,10 @@
+/* COPYRIGHT (c) 2016 Nova Labs SRL
+ *
+ * All rights reserved. All use of this software and documentation is
+ * subject to the License Agreement located in the file LICENSE.
+ */
+
+#include <Core/HW/UID.hpp>
+
+const Core::HW::UID Core::HW::UID_::_uid({Core::HW::UID_::CPU_UID(0), Core::HW::UID_::CPU_UID(1), Core::HW::UID_::CPU_UID(2), Core::HW::UID_::CPU_UID(3), Core::HW::UID_::CPU_UID(4), Core::HW::UID_::CPU_UID(5), Core::HW::UID_::CPU_UID(
+                                             6), Core::HW::UID_::CPU_UID(7), Core::HW::UID_::CPU_UID(8), Core::HW::UID_::CPU_UID(9), Core::HW::UID_::CPU_UID(10), Core::HW::UID_::CPU_UID(11)});
