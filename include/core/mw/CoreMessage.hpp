5c4a718114dc4af2af418bdbe9d98782631ba0dd
Davide Rizzi <d.rizzi@novalabs.io>
2017-05-09 18:23:55 +0200

    Added Message printf defines

--- include/core/mw/CoreMessage.hpp
+++ include/core/mw/CoreMessage.hpp
@@ -11,7 +11,9 @@
 
 /* Macros that allow us to keep the python code generator clean */
 #define CORE_MESSAGE_BEGIN(__name__)  struct __name__: \
-    public core::mw::Message {
+    public core::mw::Message { \
+public: \
+    using MessageType = __name__;
 #define CORE_MESSAGE_FIELD(__name__, __type__, __size__) \
 public: \
     core::mw::CoreTypeTraits<core::mw::CoreType::__type__, __size__>::Type __name__;
@@ -24,3 +26,14 @@ public: \
 #define CORE_MESSAGE_END \
     } \
     CORE_PACKED_ALIGNED;
+
+#define CORE_MESSAGE_FORMAT_BEGIN(__format__) \
+public: \
+    static int format(char* buffer, size_t length, const MessageType& m) { \
+        return snprintf(buffer, length, __format__
+
+#define CORE_MESSAGE_FORMAT_END \
+        ); \
+    }
+
+#define CORE_MESSAGE_FORMAT_FIELD(__field__) , m.__field__
