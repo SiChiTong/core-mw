530480090cec4844662df9b714bf7d4b2c09fa08
Davide Rizzi <d.rizzi@novalabs.io>
2017-10-04 11:11:59 +0200

    Added all messages

--- include/core/mw/BootMsg.hpp
+++ include/core/mw/BootMsg.hpp
@@ -11,37 +11,38 @@
 #include <core/mw/Message.hpp>
 #include <core/mw/NamingTraits.hpp>
 #include <core/Array.hpp>
+#include <core/String.hpp>
 
 NAMESPACE_CORE_MW_BEGIN
 
 namespace bootloader {
 using ProductUID = uint16_t;
 using ModuleUID  = uint32_t;
-using ModuleType = core::Array<char, 12>;
-using ModuleName = core::Array<char, 14>;
+using ModuleType = core::String<12>;
+using ModuleName = core::String<14>;
 
-static const uint32_t SHORT_MESSAGE_LENGTH = 8;
 static const uint32_t LONG_MESSAGE_LENGTH  = 48;
+static const uint32_t SHORT_MESSAGE_LENGTH = 8;
 
 static const uint32_t MAXIMUM_MESSAGE_LENGTH = 48;
 
 enum class MessageType : uint8_t {
     NONE           = 0x00,
-    REQUEST        = 0x01, //
-    IDENTIFY_SLAVE = 0x02, //
-    SELECT_SLAVE   = 0x10, //
-    DESELECT_SLAVE = 0x11, //
+    REQUEST        = 0x01,
+    IDENTIFY_SLAVE = 0x02,
+    SELECT_SLAVE   = 0x10,
+    DESELECT_SLAVE = 0x11,
 
     ERASE_CONFIGURATION      = 0x04,
     ERASE_PROGRAM            = 0x05,
     WRITE_PROGRAM_CRC        = 0x06,
     ERASE_USER_CONFIGURATION = 0x07,
 
-    MODULE_NAME       = 0x25,
-    READ_MODULE_NAME  = 0x26,
-    WRITE_MODULE_NAME = 0x27,
-    WRITE_MODULE_ID   = 0x28,
-    DESCRIBE          = 0x29, //
+    MODULE_NAME         = 0x25,
+    READ_MODULE_NAME    = 0x26,
+    WRITE_MODULE_NAME   = 0x27,
+    WRITE_MODULE_CAN_ID = 0x28,
+    DESCRIBE            = 0x29,
 
     IHEX_WRITE = 0x50,
     IHEX_READ  = 0x51,
@@ -73,15 +74,8 @@ enum class AcknowledgeStatus : uint8_t {
 
 struct Message:
     public core::mw::Message {
-    Message() : command(MessageType::NONE) {}
-
-    Message(
-        MessageType c
-    ) :
-        command(c)
-    {}
-
     MessageType command;
+    uint8_t     sequenceId;
 
     const Message*
     asMessage()
@@ -93,55 +87,30 @@ struct Message:
 CORE_PACKED_ALIGNED;
 
 
+struct EMPTY_PAYLOAD {};
+
 template <std::size_t _MESSAGE_LENGTH>
-struct MessageBase:
-    public Message {
+class MessageBase:
+    public Message
+{
+public:
+    using ParentType = Message;
     static const std::size_t MESSAGE_LENGTH = _MESSAGE_LENGTH;
 
-    MessageBase() : Message(MessageType::NONE), sequenceId(0) {}
-
-    MessageBase(
-        MessageType c
-    ) : Message(c), sequenceId(0)
-    {}
-
-    //MessageType command;
-    uint8_t sequenceId;
     uint8_t data[MESSAGE_LENGTH - 2];
-
-    void
-    copyTo(
-        uint8_t* buffer
-    ) const
-    {
-        const uint8_t* f = reinterpret_cast<const uint8_t*>(this);
-        uint8_t*       t = buffer;
-
-        for (std::size_t i = 0; i < _MESSAGE_LENGTH; i++) {
-            *(t++) = *(f++);
-        }
-    }
-
-    void
-    copyFrom(
-        const uint8_t* buffer
-    )
-    {
-        const uint8_t* f = buffer;
-        uint8_t*       t = reinterpret_cast<uint8_t*>(this);
-
-        for (std::size_t i = 0; i < _MESSAGE_LENGTH; i++) {
-            *(t++) = *(f++);
-        }
-    }
 }
 
 CORE_PACKED_ALIGNED;
-#if 0
+
+
+using BootMasterMsg = MessageBase<SHORT_MESSAGE_LENGTH>;
+using BootMsg       = MessageBase<LONG_MESSAGE_LENGTH>;
+
 template <typename _CONTAINER, MessageType _TYPE, typename _PAYLOAD>
-struct Message_:
-    public _CONTAINER {
-    //static const std::size_t MESSAGE_LENGTH = _CONTAINER::MESSAGE_LENGTH;
+class Message_:
+    public _CONTAINER::PARENT
+{
+public:
     using ContainerType = _CONTAINER;
     using PayloadType   = _PAYLOAD;
 
@@ -155,11 +124,146 @@ struct Message_:
 }
 
 CORE_PACKED_ALIGNED;
-#endif // if 0
 
-using BootMasterMsg = MessageBase<SHORT_MESSAGE_LENGTH>;
-using BootMsg       = MessageBase<LONG_MESSAGE_LENGTH>;
+
+struct AcknowledgeMessage:
+    public core::mw::Message {
+    MessageType       command;
+    uint8_t           sequenceId;
+    AcknowledgeStatus status;
+    MessageType       type;
+
+    const Message*
+    asMessage()
+    {
+        return this;
+    }
 }
 
+CORE_PACKED_ALIGNED;
 
+template <std::size_t _MESSAGE_LENGTH>
+class AcknowledgeMessageBase:
+    public AcknowledgeMessage
+{
+public:
+    using ParentType = AcknowledgeMessage;
+    static const std::size_t MESSAGE_LENGTH = _MESSAGE_LENGTH;
+
+    uint8_t data[MESSAGE_LENGTH - sizeof(AcknowledgeMessage)];
+}
+
+CORE_PACKED_ALIGNED;
+
+template <typename CONTAINER, typename PAYLOAD>
+class AcknowledgeMessage_:
+    public CONTAINER::ParentType
+{
+public:
+    using ContainerType = CONTAINER;
+    using PayloadType   = PAYLOAD;
+
+    PAYLOAD data;
+
+    uint8_t padding[ContainerType::MESSAGE_LENGTH - sizeof(AcknowledgeMessage) - sizeof(data)];
+}
+
+CORE_PACKED_ALIGNED;
+
+namespace payload {
+struct EMPTY {};
+
+struct NAME {
+    ModuleName name;
+};
+
+struct UID {
+    ModuleUID uid;
+};
+
+struct UIDAndMaster {
+    ModuleUID uid;
+    uint8_t   masterID;
+};
+
+struct UIDAndCRC {
+    ModuleUID uid;
+    uint32_t  crc;
+};
+
+struct UIDAndID {
+    ModuleUID uid;
+    uint8_t   id;
+};
+
+struct UIDAndName {
+    ModuleUID  uid;
+    ModuleName name;
+};
+
+struct UIDAndAddress {
+    ModuleUID uid;
+    uint32_t  address;
+};
+
+struct IHex {
+    enum Type : uint8_t {
+        BEGIN = 0x01,
+        DATA  = 0x02,
+        END   = 0x03
+    };
+
+    using Data = char[44];
+
+    Type type;
+    Data string;
+};
+
+struct Announce {
+    ModuleUID uid;
+};
+
+struct Describe {
+    uint32_t   programFlashSize;
+    uint16_t   userFlashSize;
+    uint8_t    moduleId;
+    ModuleType moduleType;
+    ModuleName moduleName;
+};
+}
+
+namespace messages {
+// SLAVE -> MASTER
+using Announce = Message_<BootMasterMsg, MessageType::REQUEST, payload::Announce>;
+
+// MASTER -> SLAVE
+using Bootload       = Message_<BootMsg, MessageType::BOOTLOAD, payload::EMPTY>;
+using BootloadByName = Message_<BootMsg, MessageType::BOOTLOAD_BY_NAME, payload::NAME>;
+
+using IdentifySlave = Message_<BootMsg, MessageType::IDENTIFY_SLAVE, payload::UID>;
+using SelectSlave   = Message_<BootMsg, MessageType::SELECT_SLAVE, payload::UIDAndMaster>;
+using DeselectSlave = Message_<BootMsg, MessageType::DESELECT_SLAVE, payload::UID>;
+
+using EraseConfiguration = Message_<BootMsg, MessageType::ERASE_CONFIGURATION, payload::UID>;
+using EraseProgram       = Message_<BootMsg, MessageType::ERASE_PROGRAM, payload::UID>;
+using WriteProgramCrc    = Message_<BootMsg, MessageType::WRITE_PROGRAM_CRC, payload::UIDAndCRC>;
+using Describe = Message_<BootMsg, MessageType::DESCRIBE, payload::UID>;
+
+
+using IHexData = Message_<BootMsg, MessageType::IHEX_READ, payload::IHex>;
+
+using IHexRead = Message_<BootMsg, MessageType::IHEX_WRITE, payload::UIDAndAddress>;
+
+using Reset = Message_<BootMsg, MessageType::RESET, payload::UID>;
+
+using ReadName = Message_<BootMsg, MessageType::READ_MODULE_NAME, payload::UID>;
+using WriteModuleName = Message_<BootMsg, MessageType::WRITE_MODULE_NAME, payload::UIDAndName>;
+using WriteModuleID   = Message_<BootMsg, MessageType::WRITE_MODULE_CAN_ID, payload::UIDAndID>;
+}
+
+using AcknowledgeMsg      = AcknowledgeMessageBase<LONG_MESSAGE_LENGTH>;
+using AcknowledgeUID      = AcknowledgeMessage_<AcknowledgeMsg, payload::UID>;
+using AcknowledgeDescribe = AcknowledgeMessage_<AcknowledgeMsg, payload::Describe>;
+using AcknowledgeString   = AcknowledgeMessage_<AcknowledgeMsg, char[44]>;
+}
 NAMESPACE_CORE_MW_END
